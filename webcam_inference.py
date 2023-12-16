4ac467fb395d1317d0c79a511c91924943b2917d
egeozgul <egeozgul@gmail.com>
2023-12-16 13:45:44 -0500

    Update webcam_inference.py

--- webcam_inference.py
+++ webcam_inference.py
@@ -1,68 +1,56 @@
+import cv2
+import numpy as np
 import tensorflow as tf
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, GlobalAveragePooling2D
-from tensorflow.keras.preprocessing.image import ImageDataGenerator, load_img, img_to_array
-import os
 
-# 1. Dataset Acquisition
-train_dir = './fruits-360/Training'
-test_dir = './fruits-360/Test'
+# Load the pre-trained MobileNetV2 model
+model = tf.keras.models.load_model('path_to_your_fruit_model.h5')
 
-# Image Data Generators
-train_datagen = ImageDataGenerator(
-    rescale=1./255,
-    rotation_range=20,
-    width_shift_range=0.2,
-    height_shift_range=0.2,
-    zoom_range=0.2,
-    horizontal_flip=True
-)
+# Initialize the webcam
+cap = cv2.VideoCapture(0)
 
-test_datagen = ImageDataGenerator(rescale=1./255)
+def segment_fruit_area(frame):
+    """Segment potential fruit areas based on color."""
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    
+    # This range should capture many fruits but might need adjustment for specific fruits or lighting conditions
+    lower_range = np.array([20, 100, 100])
+    upper_range = np.array([160, 255, 255])
+    
+    mask = cv2.inRange(hsv, lower_range, upper_range)
+    contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    
+    # Return the largest contour (if any)
+    if contours:
+        return max(contours, key=cv2.contourArea)
+    return None
 
-train_generator = train_datagen.flow_from_directory(
-    train_dir,
-    target_size=(224, 224),
-    batch_size=32,
-    class_mode='categorical'
-)
+while True:
+    ret, frame = cap.read()
 
-test_generator = test_datagen.flow_from_directory(
-    test_dir,
-    target_size=(224, 224),
-    batch_size=32,
-    class_mode='categorical',
-    shuffle=False
-)
+    contour = segment_fruit_area(frame)
+    
+    if contour is not None:
+        # Compute the bounding rectangle of the contour to crop
+        x, y, w, h = cv2.boundingRect(contour)
+        cropped = frame[y:y+h, x:x+w]
+        cropped_resized = cv2.resize(cropped, (224, 224))
+        
+        image = tf.convert_to_tensor(cropped_resized, dtype=tf.float32) / 255.0
+        image = tf.expand_dims(image, axis=0)
+        
+        predictions = model.predict(image)
+        predicted_class = np.argmax(predictions, axis=1)
+        confidence = predictions[0][predicted_class[0]]
 
-# 2. Model Selection
-base_model = tf.keras.applications.MobileNetV2(input_shape=(224, 224, 3), include_top=False, weights='imagenet')
-base_model.trainable = False
+        # If the classifier confirms there's a fruit, draw the contour
+        THRESHOLD = 0.9
+        if confidence > THRESHOLD:
+            cv2.drawContours(frame, [contour], -1, (0, 0, 255), 2)
 
-model = Sequential([
-    base_model,
-    GlobalAveragePooling2D(),
-    Dense(len(train_generator.class_indices), activation='softmax')
-])
+    cv2.imshow('Fruit Detector', frame)
+    
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
 
-model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
-
-# 3. Training
-model.fit(train_generator, epochs=10, validation_data=test_generator)
-
-# 4. Inference
-#def predict_fruit(image_path):
-#    image = load_img(image_path, target_size=(224, 224))
-#    image = img_to_array(image) / 255.0
-#    image = tf.expand_dims(image, axis=0)
-#    
-#    prediction = model.predict(image)
-#    predicted_class = tf.argmax(prediction, axis=1).numpy()
-#    
-#    # Map class index to class label
-#    class_labels = list(train_generator.class_indices.keys())
-#    return class_labels[predicted_class[0]]
-#
-# Example usage:
-# image_path = "path_to_your_image.jpg"
-# print(predict_fruit(image_path))
+cap.release()
+cv2.destroyAllWindows()
